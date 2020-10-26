f7b93404726f4a64e980782f37df5285d8fb4ca2
Carlos Villalvazo <zentrix.24@gmail.com>
2020-10-25 23:05:21 -0600

    add card components

--- src/components/card/CardFront.vue
+++ src/components/card/CardFront.vue
@@ -1,12 +1,35 @@
 <template>
     <div class="row"> 
-        <div class="col-sm-6 card edit-area"></div>
-        <div class="col-sm-6 card edit-display"></div>
+        <div class="col-sm-6 card edit-area">
+            <cc-text-input @displayTextChanged="textBoxValue1 = $event"></cc-text-input>
+            <cc-text-input @displayTextChanged="textBoxValue2 = $event"></cc-text-input>
+            <cc-text-input @displayTextChanged="textBoxValue3 = $event"></cc-text-input>
+        </div>
+        <div class="col-sm-6 card card-display">
+            <cc-text-ouput></cc-text-ouput>
+            <cc-text-ouput></cc-text-ouput>
+            <cc-text-ouput></cc-text-ouput>
+        </div>
     </div>    
 </template>
 
 <script>
+import TextInput from './TextInput.vue'
+import TextOuput from './TextOuput.vue'
 
+export default {
+    data: function() {
+        return {
+            textBoxValue1: '',
+            textBoxValue2: '',
+            textBoxValue3: ''
+        }
+    },
+    components: {
+        ccTextInput: TextInput, 
+        ccTextOuput: TextOuput
+    }
+}
 </script>
 
 <style>
@@ -17,7 +40,7 @@
     height: 800px;
 }
 
-.edit-display{
+.card-display{
     height: 800px;
     padding: 20px;
 }
---
+++ src/components/card/CardInsideLeft.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="row"> 
+        <div class="col-sm-6 card edit-area">
+            <p> CardInsideLeft</p>
+        </div>
+        <div class="col-sm-6 card edit-display"></div>
+    </div>    
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+.edit-area{
+    background-color: #d2d9d9;
+    padding: 20px;
+    height: 800px;
+}
+
+.edit-display{
+    height: 800px;
+    padding: 20px;
+}
+</style>
