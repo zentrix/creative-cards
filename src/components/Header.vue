eb50d3c392bce661f4d1c975005ee6b5f3fb2835
Carlos Villalvazo <zentrix.24@gmail.com>
2020-10-25 22:14:08 -0600

    creation of the cardFront component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,16 +2,29 @@
     <div>
         <h1 class="text-center">Creative cards</h1>
         <ul class="nav justify-content-center">
-            <li class="nav-item" >From</li>
-            <li class="nav-item" >Inside Left</li>
-            <li class="nav-item" >Inside Right</li>
-            <li class="nav-item" >Back</li>
+            <li class="nav-item" ><a class="nav-link" @click="pageSelected='cardFront'">Front</a></li>
+            <li class="nav-item" ><a class="nav-link" @click="pageSelected='cardInsideLeft'">Inside Left</a></li>
+            <li class="nav-item" ><a class="nav-link" @click="pageSelected='cardInsideRight'">Inside Right</a></li>
+            <li class="nav-item" ><a class="nav-link" @click="pageSelected='cardBack'">Back</a></li>
         </ul>
     </div>
 </template>
 
 <script>
+    export default {
+        data: function() {
+            return{
+                pageSelected: ''
+            }
+        }, 
+        watch: { //the watch are properties no functions
 
+            // this how send data to the parent component
+            pageSelected: function(){
+                this.$emit('pageWasChanged', this.pageSelected)
+            }
+        }
+    }
 </script>
 
 <style>
---
+++ src/components/card/CardFront.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="row"> 
+        <div class="col-sm-6 card edit-area"></div>
+        <div class="col-sm-6 card edit-display"></div>
+    </div>    
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+.edit-area{
+    background-color: #d2d9d9;
+    padding: 20px;
+    height: 800px;
+}
+
+.edit-display{
+    height: 800px;
+    padding: 20px;
+}
+</style>
