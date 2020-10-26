190c3dca050ca8c8dba055ac286870fa26dc7a6e
Carlos Villalvazo <zentrix.24@gmail.com>
2020-10-26 03:11:15 +0000

    add navHeader component

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1 class="text-center">Creative cards</h1>
+        <ul class="nav justify-content-center">
+            <li class="nav-item" >From</li>
+            <li class="nav-item" >Inside Left</li>
+            <li class="nav-item" >Inside Right</li>
+            <li class="nav-item" >Back</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+    h1{
+        margin-top: 25px;
+    }
+</style>
