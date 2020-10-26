cf78772d47112b8a22a7c85daf2351baca997734
Carlos Villalvazo <zentrix.24@gmail.com>
2020-10-25 23:06:23 -0600

    add textinput & textOuput component

---
+++ src/components/card/TextInput.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="from-group">
+                <p>Edit text:</p>
+                <textarea class="from-group" cols="50" rows="4" 
+                            placeholder="Add text here!" 
+                            v-model="textBoxInput"
+                            @keyup="textChanged"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function(){
+            return{
+                textBoxInput: ""
+            }
+        }, 
+        methods: {
+            textChanged: function(){
+                this.$emit('displayTextChanged', this.textBoxInput)            
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
---
+++ src/components/card/TextOuput.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p> Sample Text</p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+<style>
+
+</style>
